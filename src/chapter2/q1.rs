4ce833b6135233d087487c48fc1fc70ecf067adc
Filip Degmečić <filip_degmecic@yahoo.com>
2023-09-18 21:19:34 +0200

    chapter 2 question 1

--- src/chapter2/q1.rs
+++ src/chapter2/q1.rs
@@ -2,6 +2,10 @@
 #![allow(dead_code)]
 
 use std::cell::RefCell;
+use std::collections::HashSet;
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::hash::Hash;
 use std::rc::Rc;
 
 type Link<T> = Option<Rc<RefCell<Node<T>>>>;
@@ -11,41 +15,78 @@ struct Node<T> {
     value: T,
 }
 
-impl<T> Node<T> {
-    fn new(value: T) -> Rc<RefCell<Node<T>>> {
-        Rc::new(RefCell::new(Node { next: None, value }))
-    }
-}
-
 struct LinkedList<T> {
     head: Link<T>,
 }
 
-impl<T> LinkedList<T> {
+impl<T> LinkedList<T>
+where
+    T: Clone + Eq + Hash + Display,
+{
     fn new() -> Self {
         LinkedList { head: None }
     }
 
     fn append(&mut self, value: T) {
-        let new_node = Node::new(value);
-
-        if let Some(old_head) = self.head.take() {
-            old_head.borrow_mut().next = Some(new_node.clone())
-        }
+        let new_node = Rc::new(RefCell::new(Node {
+            next: self.head.take(),
+            value,
+        }));
 
         self.head = Some(new_node);
     }
 
-    fn remove_duplicates(&mut self) {}
+    fn remove_duplicates(&mut self) {
+        let mut set = HashSet::new();
+
+        let mut prev: Link<T> = None;
+        for node in self.iter() {
+            let value = node.borrow().value.clone();
+
+            if set.contains(&value) {
+                if let Some(previous) = prev.as_ref() {
+                    previous.borrow_mut().next = node.borrow().next.clone();
+                }
+            } else {
+                set.insert(value);
+                prev = Some(node.clone());
+            }
+        }
+    }
+
+    fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.clone(),
+        }
+    }
 }
 
-struct Iter<T>(Rc<RefCell<Node<T>>>);
+#[allow(unused_must_use)]
+impl<T: Display> Display for LinkedList<T> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let mut current = self.head.clone();
+        write!(f, "{}", "[");
+        while current.is_some() {
+            write!(f, "{}", current.clone().unwrap().borrow().value);
+            current = current.unwrap().borrow_mut().next.clone();
+
+            if current.is_some() {
+                write!(f, "{}", ", ");
+            }
+        }
+        write!(f, "{}", "]")
+    }
+}
+
+struct Iter<T> {
+    next: Link<T>,
+}
 
 impl<T> Iterator for Iter<T> {
     type Item = Rc<RefCell<Node<T>>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.next().take() {
+        match self.next.take() {
             Some(curr) => {
                 self.next = curr.borrow().next.clone();
                 Some(curr)
@@ -57,10 +98,19 @@ impl<T> Iterator for Iter<T> {
 
 #[cfg(test)]
 mod tests {
-    use super::{LinkedList, Node};
+    use super::LinkedList;
 
     #[test]
     fn should_remove_duplicates() {
-        let mut list = LinkedList::new().append(1);
+        let mut list = LinkedList::new();
+        list.append(1);
+        list.append(2);
+        list.append(2);
+        list.append(4);
+        list.append(5);
+        list.append(6);
+        list.append(6);
+        list.remove_duplicates();
+        print!("{}", list);
     }
 }
