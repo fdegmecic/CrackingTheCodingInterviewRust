0322294bbd2c7c37aa9163a109671fa8e88acb40
Filip Degmečić <filip_degmecic@yahoo.com>
2023-10-19 19:10:20 +0200

    chapter 4 question 6

--- src/chapter4/q6.rs
+++ src/chapter4/q6.rs
@@ -16,22 +16,32 @@ struct BinaryNode {
 fn get_in_order_successor(node: Link) -> Option<Link> {
     if node.borrow().right.is_some() {
         return get_left_most_child(node.borrow().right.clone());
-    } else {
-        let mut current_node = node;
-        let mut parent = current_node.borrow().parent.clone();
-
-        while parent.is_some() && current_node == parent.as_ref().unwrap().borrow().right.clone()? {
-            current_node = parent.unwrap();
-            parent = current_node.borrow().parent.clone();
-        }
-        return parent;
     }
+
+    get_parent(node)
 }
 
 fn get_left_most_child(node: Option<Link>) -> Option<Link> {
     let iter = LeftMostIter { next: node };
 
     iter.last()
+}
+
+fn get_parent(node: Link) -> Option<Link> {
+
+
+    let node = node.clone();
+    let parent = node.borrow().parent.clone();
+
+    if let Some(parent) = parent {
+        return if node.borrow().value < parent.borrow().value {
+            Some(parent)
+        } else {
+            get_parent(parent)
+        }
+    }
+
+    None
 }
 
 struct LeftMostIter {
@@ -49,6 +59,18 @@ impl Iterator for LeftMostIter {
     }
 }
 
+impl BinaryNode {
+
+    fn new(value: usize) -> Link {
+        Rc::new(RefCell::new(BinaryNode {
+            value,
+            parent: None,
+            left: None,
+            right: None,
+        }))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::{cell::RefCell, rc::Rc};
@@ -56,62 +78,33 @@ mod tests {
     use crate::chapter4::q6::{get_in_order_successor, BinaryNode};
 
     #[test]
-    fn should_get_in_order_successor() {}
-
-    #[test]
-    fn test_in_order_successor() {
-        // Creating a valid binary tree:
+    fn should_get_in_order_successor() {
         //       4
         //      / \
         //     2   6
         //    / \
         //   1   3
-        let root = Rc::new(RefCell::new(BinaryNode {
-            value: 4,
-            parent: None,
-            left: None,
-            right: None,
-        }));
 
-        let node1 = Rc::new(RefCell::new(BinaryNode {
-            value: 1,
-            parent: Some(root.clone()),
-            left: None,
-            right: None,
-        }));
-
-        let node3 = Rc::new(RefCell::new(BinaryNode {
-            value: 3,
-            parent: Some(root.clone()),
-            left: None,
-            right: None,
-        }));
-
-        let node2 = Rc::new(RefCell::new(BinaryNode {
-            value: 2,
-            parent: Some(root.clone()),
-            left: Some(node1.clone()),
-            right: Some(node3.clone()),
-        }));
-
-        let node6 = Rc::new(RefCell::new(BinaryNode {
-            value: 6,
-            parent: Some(root.clone()),
-            left: None,
-            right: None,
-        }));
+        let root = BinaryNode::new(4);
+        let node1 = BinaryNode::new(1);
+        let node2 = BinaryNode::new(2);
+        let node3 = BinaryNode::new(3);
+        let node6 = BinaryNode::new(6);
 
         root.borrow_mut().left = Some(node2.clone());
         root.borrow_mut().right = Some(node6.clone());
+        node1.borrow_mut().parent = Some(node2.clone());
+        node3.borrow_mut().parent = Some(node2.clone());
+        node2.borrow_mut().parent = Some(root.clone());
+        node2.borrow_mut().left = Some(node1.clone());
+        node2.borrow_mut().right = Some(node3.clone());
 
-        // Now, let's test the in-order successor of some nodes.
         let successor1 = get_in_order_successor(node1.clone());
         let successor2 = get_in_order_successor(node2.clone());
         let successor3 = get_in_order_successor(node3.clone());
         let successor4 = get_in_order_successor(root.clone());
         let successor6 = get_in_order_successor(node6.clone());
 
-        // Verify the in-order successor for each node.
         assert_eq!(successor1.unwrap().borrow().value, 2);
         assert_eq!(successor2.unwrap().borrow().value, 3);
         assert_eq!(successor3.unwrap().borrow().value, 4);
