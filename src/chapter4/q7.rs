cac48e260ce0f101fed6a5194f0a1eb2bf433473
Filip Degmečić <filip_degmecic@yahoo.com>
2023-11-11 17:01:56 +0100

    chapter 4 question 7

--- src/chapter4/q7.rs
+++ src/chapter4/q7.rs
@@ -18,7 +18,6 @@ where
 
     while let Some(node) = state.no_deps.pop() {
         res.push(node);
-
         if let Some(dependents) = state.get_dependents(&node) {
             for dependent in dependents.clone() {
                 state.resolve(&dependent, &node);
@@ -39,18 +38,22 @@ where
 
 type Graph<T> = HashMap<T, HashSet<T>>;
 
-fn add_edge<T>(graph: &mut Graph<T>, from: T, to: T)
+fn add_edge<T>(graph: &mut Graph<T>, from: T, to: Option<T>)
 where
     T: Eq + Hash + Copy,
 {
     graph
         .entry(from)
         .and_modify(|pointees| {
-            pointees.insert(to);
+            if let Some(node) = to {
+                pointees.insert(node);
+            }
         })
         .or_insert_with(|| {
             let mut s = HashSet::new();
-            s.insert(to);
+            if let Some(node) = to {
+                s.insert(node);
+            }
             s
         });
 }
@@ -59,6 +62,16 @@ struct State<T> {
     depends_on: Graph<T>,
     dependents: Graph<T>,
     no_deps: Vec<T>,
+}
+
+impl<T> Default for State<T> {
+    fn default() -> Self {
+        Self {
+            depends_on: HashMap::new(),
+            dependents: HashMap::new(),
+            no_deps: vec![],
+        }
+    }
 }
 
 impl<T> State<T>
@@ -95,9 +108,52 @@ where
     }
 }
 
+impl<T> From<&Graph<T>> for State<T>
+where
+    T: Eq + Hash + Copy,
+{
+    fn from(graph: &Graph<T>) -> Self {
+        let mut state = State::default();
+
+        for (&node, dependencies) in graph.iter() {
+            if dependencies.is_empty() {
+                state.no_deps.push(node);
+            } else {
+                for &dependency in dependencies {
+                    add_edge(&mut state.depends_on, node, Some(dependency));
+                    add_edge(&mut state.dependents, dependency, Some(node));
+                }
+            }
+        }
+
+        state
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::chapter4::q7::*;
 
     #[test]
-    fn toplogical_sort() {}
+    fn should_topological_sort() {
+        let mut graph: Graph<char> = HashMap::new();
+        add_edge(&mut graph, 'f', Some('c'));
+        add_edge(&mut graph, 'f', Some('b'));
+        add_edge(&mut graph, 'f', Some('a'));
+        add_edge(&mut graph, 'c', Some('a'));
+        add_edge(&mut graph, 'b', Some('a'));
+        add_edge(&mut graph, 'b', Some('e'));
+        add_edge(&mut graph, 'a', Some('e'));
+        add_edge(&mut graph, 'd', Some('g'));
+        add_edge(&mut graph, 'e', None);
+        add_edge(&mut graph, 'g', None);
+
+        let result = topological_sort(&graph).unwrap();
+        let sorted = result.iter().rev().collect::<Vec<&char>>();
+
+        assert_eq!(
+            "['f', 'c', 'b', 'a', 'e', 'd', 'g']",
+            format!("{:?}", sorted)
+        );
+    }
 }
