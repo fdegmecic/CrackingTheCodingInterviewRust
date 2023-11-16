e50a3f5ac258cc512b9fa2969b308495c1b1212b
Filip Degmečić <filip_degmecic@yahoo.com>
2023-11-16 21:31:50 +0100

    chapter 4 question 8

--- src/chapter4/q8.rs
+++ src/chapter4/q8.rs
@@ -33,7 +33,70 @@ fn r_lca(root: &Option<TreeNode>, p: usize, q: usize) -> Option<TreeNode> {
 
 #[cfg(test)]
 mod tests {
+    use std::{cell::RefCell, rc::Rc};
+
+    use crate::chapter4::q2::BinaryNode;
+
+    use super::{lowest_common_ancestor, TreeNode};
 
     #[test]
-    fn should_find_lca() {}
+    fn should_find_lca() {
+        //       2
+        //      /\
+        //     10 30
+        //    / \
+        //   55  15
+        //  /  \   \
+        // 3   37   17
+
+        let tree = get_tree();
+
+        let result = lowest_common_ancestor(Some(tree), 37, 17);
+        assert_eq!(result.unwrap(), 10);
+    }
+
+    fn get_tree() -> TreeNode {
+        let leaf_1 = Rc::new(RefCell::new(BinaryNode {
+            value: 3,
+            left: None,
+            right: None,
+        }));
+        let leaf_2 = Rc::new(RefCell::new(BinaryNode {
+            value: 37,
+            left: None,
+            right: None,
+        }));
+        let leaf_3 = Rc::new(RefCell::new(BinaryNode {
+            value: 17,
+            left: None,
+            right: None,
+        }));
+        let leaf_4 = Rc::new(RefCell::new(BinaryNode {
+            value: 55,
+            left: Some(leaf_1.clone()),
+            right: Some(leaf_2.clone()),
+        }));
+        let leaf_5 = Rc::new(RefCell::new(BinaryNode {
+            value: 15,
+            left: None,
+            right: Some(leaf_3.clone()),
+        }));
+        let leaf_6 = Rc::new(RefCell::new(BinaryNode {
+            value: 10,
+            left: Some(leaf_4.clone()),
+            right: Some(leaf_5.clone()),
+        }));
+        let leaf_7 = Rc::new(RefCell::new(BinaryNode {
+            value: 30,
+            left: None,
+            right: None,
+        }));
+        let root = Rc::new(RefCell::new(BinaryNode {
+            value: 4,
+            left: Some(leaf_6.clone()),
+            right: Some(leaf_7.clone()),
+        }));
+
+        root
+    }
 }
